a1835ebce4f7d20540cfd7bf49769e1df7d500ba
weedge <weege007@gmail.com>
2022-01-06 23:23:59 +0800

    change shop-admin-backend page tpl use edge

--- shop-admin-backend/app/Controllers/Http/ShopItemsController.ts
+++ shop-admin-backend/app/Controllers/Http/ShopItemsController.ts
@@ -2,6 +2,7 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { schema, rules } from "@ioc:Adonis/Core/Validator";
 import Database from '@ioc:Adonis/Lucid/Database';
 import ShopItem from 'App/Models/ShopItem';
+import Users from 'App/Models/User';
 import UserItem from 'App/Models/UserItem';
 
 export default class ShopItemsController {
@@ -67,26 +68,56 @@ export default class ShopItemsController {
 
     // get /items/:id/edit
     public async edit(ctx: HttpContextContract) {
-
-
-        return "edit"
+        const item = await ShopItem.find(ctx.params.id)
+        return ctx.view.render('edit', { item })
     }
 
     // put /items/:id
     public async update(ctx: HttpContextContract) {
+        const item = await ShopItem.find(ctx.params.id)
 
-        return "update"
+        if (item) {
+            item.name = ctx.request.input('name')
+            item.desc = ctx.request.input('desc')
+            item.price = ctx.request.input('price')
+            item.stock = ctx.request.input('stock')
+            if (await item.save()) {
+                const user = await ctx.auth.authenticate()
+                const uItem = new UserItem()
+                uItem.userId = user.id
+                uItem.shopItemId = item.id
+                //uItem.save()
+                await uItem.save()
+                return ctx.view.render('show', { item })
+            }
+            return // 422
+        }
+
+        return // 401
     }
 
     // delete /items/:id
     public async destroy(ctx: HttpContextContract) {
+        const user = await ctx.auth.authenticate()
+        if (user.isAdmin) {
+            await ShopItem.query().where('id', ctx.params.id).delete()
+            return ctx.response.redirect('/home')
+        }
 
-        return "destroy"
+        return // 500
     }
 
     //==== other controller method to op resource====
 
     public async byUid(ctx: HttpContextContract) {
-        return "byUid"
+        const user = await ctx.auth.authenticate()
+        const uItem = await UserItem.query().where('user_id', '=', user.id)
+        let itemIds = Array()
+        uItem.forEach(element => {
+            itemIds.push(element.id)
+        });
+        const items = await ShopItem.findMany(itemIds)
+
+        return ctx.view.render('home', { items })
     }
 }
