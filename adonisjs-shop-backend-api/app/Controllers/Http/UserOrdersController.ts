13d74088b8bffbccecfbb8e234698cf02dc6b284
weedge <weege007@gmail.com>
2022-01-12 13:05:50 +0800

    add buy store

--- adonisjs-shop-backend-api/app/Controllers/Http/UserOrdersController.ts
+++ adonisjs-shop-backend-api/app/Controllers/Http/UserOrdersController.ts
@@ -1,12 +1,14 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import UserOrder from 'App/Models/UserOrder'
 import Event from '@ioc:Adonis/Core/Event'
+import ShopItem from 'App/Models/ShopItem'
+import User from 'App/Models/User'
 
 export default class UserOrdersController {
     public async order(ctx: HttpContextContract) {
         const user = await ctx.auth.authenticate()
         const order = new UserOrder()
-        order.itemId = ctx.request.input('itemId')
+        order.itemId = parseInt(ctx.request.input('itemId'))
         order.ext = "new shop item_id:" + order.itemId
         //order.userId = ctx.request.input('userId')
         order.userId = user.id
@@ -20,8 +22,13 @@ export default class UserOrdersController {
     public async info(ctx: HttpContextContract) {
         //@TODO cache detail info
         //const user = await ctx.auth.authenticate()
-        const item = await UserOrder.find(ctx.params.orderId)
-        return item
+        const order = await UserOrder.find(ctx.params.orderId)
+        let item: any
+        if (order) {
+            item = await ShopItem.find(order.itemId)
+        }
+
+        return { order, item }
     }
 
     public async list(ctx: HttpContextContract) {
@@ -30,13 +37,23 @@ export default class UserOrdersController {
         // return ctx.response.status(520).json({ "error":"not self"})
         // }
         //@TODO page
-        const items = await UserOrder.query()
+        const orders = await UserOrder.query()
             .select('*')
             .where((query) => {
                 //query.where("user_id", ctx.params.uid)
                 query.where("user_id", user.id)
             }).orderBy('order_id', 'desc')
 
-        return items
+        let itemIds = Array()
+        orders.forEach(element => {
+            itemIds.push(element.itemId)
+        });
+        const items = await ShopItem.findMany(itemIds)
+        let itemMap = new Map()
+        items.forEach(element => {
+            itemMap[element.id] = element
+        });
+
+        return { orders, itemMap }
     }
 }
